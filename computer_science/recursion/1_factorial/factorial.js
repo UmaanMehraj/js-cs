0fa2522c56a47150033031ec66b8e6518a9338c9
UmaanMehraj <umaanmehraj5@gmail.com>
2026-04-13 09:11:32 +0530

    sol: 1_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,19 @@
-const factorial = function() {
-
-};
+const factorial = function (num) {
+  if (!Number.isInteger(num)) {
+    return undefined
+  } else {
+    if (num >= 0) {
+      if (num === 1 || num === 0) {
+        return 1
+      } else {
+        return num * factorial(num - 1)
+      }
+    }
+    else{
+        return undefined
+    }
+  }
+}
 
 // Do not edit below this line
-module.exports = factorial;
+module.exports = factorial
